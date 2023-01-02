7fd5fe59ead0fbca5a8212c2171d5b4a250955d2
tsunoda <tsunoda@aomori-u.ac.jp>
2023-01-02 22:38:13 +0900

    イベントハンドリング用クラス実装

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-// import 'package:syncfusion_flutter_calendar/calendar.dart';
+import 'package:syncfusion_flutter_calendar/calendar.dart';
 // import 'package:intl/intl.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 // import 'package:googleapis_auth/googleapis_auth.dart';
@@ -315,5 +315,50 @@ class _MyHomePageState extends State<MyHomePage> {
         },
      ),
     );
+  }
+}
+
+class GoogleDataSource extends CalendarDataSource {
+  GoogleDataSource({required List<Event> events}) {
+    this.appointments = events;
+  }
+
+  @override
+  DateTime getStartTime(int index) {
+    final Event event = appointments![index];
+    return event.start!.date ?? event.start!.dateTime!.toLocal();
+  }
+
+  @override
+  bool isAllDay(int index) {
+    return appointments![index].start.date != null;
+  }
+
+  @override
+  DateTime getEndTime(int index) {
+    final Event event = appointments![index];
+    return event.endTimeUnspecified != null && event!.endTimeUnspecified!
+        ? (event.start!.date ?? event.start!.dateTime!.toLocal())
+        : (event.end!.date != null
+        ? event.end!.date!.add(Duration(days: -1))
+        : event.end!.dateTime!.toLocal());
+  }
+
+  @override
+  String getLocation(int index) {
+    return appointments![index].location;
+  }
+
+  @override
+  String getNotes(int index) {
+    return appointments![index].description;
+  }
+
+  @override
+  String getSubject(int index) {
+    final Event event = appointments![index];
+    return event.summary == null || event.summary!.isEmpty
+        ? 'No Title'
+        : event.summary!;
   }
 }
